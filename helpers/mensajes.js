6d6ac0d4d28d3e349ba40bd10d804a9906ad9f96
Emanuel503 <emanueljosemolina@hotmail.es>
2023-05-05 10:38:55 -0600

    Inicio del proyecto

---
+++ helpers/mensajes.js
@@ -0,0 +1,11 @@
+require('colors')
+
+const mostrarMenu = () =>{
+    console.log("========================". blue);
+    console.log("  SELECCION UNA OPCIÓN  ". blue);
+    console.log("========================". blue);
+}
+
+module.exports = {
+    mostrarMenu
+}
---
+++ notas.md
@@ -0,0 +1,3 @@
+Notas del video
+
+*Primero se importan las librerias de tercero y luego las liberias propias
